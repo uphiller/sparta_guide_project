58766195e862499296dd4332770da488b3c9b9c2
곽영호 <1080609@hanwha.com>
2020-11-15 21:25:52 +0900

    api 추가

--- app.py
+++ app.py
@@ -9,5 +9,21 @@ db = client.dbsparta
 def home():
     return render_template('index.html')
 
+@app.route('/place', methods=['POST'])
+def save_place():
+    x_receive = request.form['x_give']
+    y_receive = request.form['y_give']
+    comment_receive = request.form['comment_give']
+    place = {'x': x_receive, 'y': y_receive, 'comment':comment_receive}
+    db.places.insert_one(place)
+    return jsonify({'result': 'success'})
+
+@app.route('/place', methods=['GET'])
+def show_place():
+    x_receive = request.args.get('x_give')
+    y_receive = request.args.get('y_give')
+    place = db.places.find_one({'x': x_receive, 'y': y_receive}, {'_id': 0})
+    return jsonify({'result': 'success', 'place': place})
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
